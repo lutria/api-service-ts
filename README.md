e483f5a3123572ebe0df1e0140e03204fc1502ee
Dave Hughes <me@davehughes.net>
2023-01-16 22:04:56 +0000

    Some error handling

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ npx prisma format
 ```shell
 npx prisma generate
 ```
+
+
+https://www.prisma.io/blog/client-extensions-preview-8t3w27xkrxxn
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Prisma } from '@prisma/client'
 import express from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { forUser } from './security'
 
 const prisma = new PrismaClient()
@@ -9,10 +10,21 @@ app.use(express.json())
 
 // Middleware to create extended Prisma client with data security
 app.use(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  // TODO: Handle missing user header and non-existent user
-  const user = await prisma.user.findUniqueOrThrow({ where: { name: req.header('x-user') } })
-  req.xprisma = prisma.$extends(forUser({ privileged: user.privileged }))
-  next();
+  const userFromHeader = req.header('x-user')
+  if (!userFromHeader) {
+    return res.status(401).json({ error: "No user specified"})
+  }
+
+  let user;
+  try {
+    user = await prisma.user.findUniqueOrThrow({ where: { name: req.header('x-user') } })
+  } catch (error) {
+    console.log(error)
+    return res.status(401).json({ error: "User not found" })
+  }
+
+  req.xprisma = prisma.$extends(forUser(user))
+  return next();
 })
 
 app.get('/providers', async (req, res) => {
@@ -25,21 +37,23 @@ app.get('/provider/:id', async (req, res) => {
 
   const provider = await req.xprisma.provider.findUnique({ where: { id }})
 
+  if (provider === null) {
+    return res.status(404).json({ error: "Provider not found" })
+  }
+
   res.json(provider)
 })
 
-app.get('/provider/:id/sources', async (req, res) => {
+app.get('/provider/:id/sources', async (req, res, next) => {
   const { id } = req.params
 
-  const sources = await req.xprisma.provider
-    .findUniqueOrThrow({
-      where: {
-        id,
-      }
-    })
-    .sources()
+  const sources = await req.xprisma.provider.findUnique({ where: { id } }).sources()
 
-  res.json(sources)
+  if (sources === null) {
+    return res.status(404).json({ error: "Provider not found" })
+  }
+
+  return res.json(sources)
 })
 
 app.get('/source/:id/items', async (req, res) => {
@@ -53,6 +67,10 @@ app.get('/source/:id/items', async (req, res) => {
     })
     .mediaItems()
 
+  if (items === null) {
+    return res.status(404).json({ error: "Source not found" })
+  }
+
   res.json(items)
 })
 
@@ -61,7 +79,16 @@ app.get('/item/:id', async (req, res) => {
 
   const item = await req.xprisma.mediaItem.findUnique({ where: { id }})
 
+  if (item === null) {
+    return res.status(404).json({ error: "Item not found" })
+  }
+
   res.json(item)
+})
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack)
+  return res.status(500).json({ error: `Unexpected error: ${err.message}` })
 })
 
 const server = app.listen(3000, () => {
